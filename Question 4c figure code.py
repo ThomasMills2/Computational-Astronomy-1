a9ae924be73e872f512be54e15b0a7d72c51a0fa
ThomasMills2 <thomasamills25@gmail.com>
2025-11-01 22:46:45 +0000

    Add files via upload

---
+++ Question 4c figure code.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  8 21:08:31 2023
+
+@author: Tom
+"""
+
+import pandas as pd
+import matplotlib.pyplot as plt 
+import numpy as np
+from astropy.cosmology import FlatLambdaCDM
+
+cosmo = FlatLambdaCDM(H0=70, Om0=0.3, Tcmb0=2.725)
+df = pd.read_csv('galaxy_data.csv')
+lumDist = np.array(cosmo.luminosity_distance(df['redshift']))
+
+lumDist_cm = lumDist*(3.086*10**24)
+
+fig, ax = plt.subplots()
+
+data = pd.read_csv('galaxy_data.csv')
+
+redshift = data['redshift']
+F_HAlpha = (data['HAlpha_flux'])*10**-17
+
+L_HAlpha = 4*np.pi*(lumDist_cm**2)*F_HAlpha
+
+galaxy_a = (0.0728652, 3.199353103*10**40)
+galaxy_b = (0.09639389, 1.490969734*10**40)
+plt.scatter(redshift, L_HAlpha, s=1)
+plt.scatter(galaxy_a[0], galaxy_a[1], s=20, c='black', label='Galaxy A')
+plt.scatter(galaxy_b[0], galaxy_b[1], s=20, c='orange', label='Galaxy B')
+
+plt.xlabel('redshift')
+plt.ylabel('Luminosity, L_HAlpha, (ergs^-1)')
+plt.title('Luminosity Versus Redshift')
+plt.legend()
+plt.yscale('log')
+plt.grid()
+plt.show()
---
+++ Question 4d figure code.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  9 10:42:27 2023
+
+@author: Tom
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+data = pd.read_csv('galaxy_data.csv')
+
+
+data = data[(data['OIII_5007_flux']/data['OIII_5007_flux_err'] > 5) & (data['HBeta_flux']/data['HBeta_flux_err'] > 5) & (data['NII_6584_flux']/data['NII_6584_flux_err'] > 5) & (data['HAlpha_flux']/data['HAlpha_flux_err'] > 5)] 
+ 
+NII_6584_flux = data['NII_6584_flux']
+HAlpha_flux = data['HAlpha_flux']
+OIII_5007_flux = data['OIII_5007_flux']
+HBeta_flux = data['HBeta_flux']
+
+NII_HAlpha = np.log10(NII_6584_flux/HAlpha_flux)
+OIII_HBeta = np.log10(OIII_5007_flux/HBeta_flux)
+
+x = np.linspace(-2, 1, 10000)
+
+def kewley_line(x):
+    return 0.61/(x-0.47)+1.19
+
+def kauffmann_line(x):
+    return 0.61/(x-0.05)+1.3
+
+x = np.linspace(-2, 0.05, 10000)
+
+fig, ax = plt.subplots(figsize=(8,6))
+ax.scatter(NII_HAlpha, OIII_HBeta, s=3, c='blue')
+
+plt.plot(x,kewley_line(x), '--', c='red')
+plt.plot(x,kauffmann_line(x), '--', c='orange')
+
+plt.tick_params(axis='x', which='both', length=0)
+
+ax.set_xlabel(r'$\log_{10}([\rm{N\ II}]/HAlpha)$')
+ax.set_ylabel(r'$\log_{10}([\rm{O\ III}]/HBeta)$')
+plt.title('BPT Diagram for all Galaxies that have Flux/Flux_error>5')
+plt.xlim(-2,1)
+plt.ylim(-1.5, 1.5)
+plt.annotate('AGN', (0.1,1), fontsize=15, color='red')
+plt.annotate('Star-Forming Galaxies', (-1.4,-0.5), fontsize=14, color='orange')
+plt.show()
+            
